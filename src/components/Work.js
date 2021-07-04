093db2aa71018bf03f09262b07538420af76b165
Li Xin Rui <li.xinrui3@gmail.com>
2021-07-03 20:54:02 -0400

    overlapping text and divs fixed

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,31 +6,31 @@ import data from "../yourdata"
 
 const Work = () => {
   return (
-    <div className="section bg-image background" id="work">
-      <div id="stars" />
-      <div id="stars2" />
-      <div id="stars3" />
-      <div className="container">
-        <div className="work-wrapper">
-          <Fade bottom>
-            <h1>Projects</h1>
-          </Fade>
+    <div className="section bg-image background bg-responsive" id="work">
+        <div id="stars" />
+        <div id="stars2" />
+        <div id="stars3" />
+        <div className="container">
+          <div className="work-wrapper">
+            <Fade bottom>
+              <h1>Projects</h1>
+            </Fade>
 
-          <div className="grid">
-            <Fade bottom cascade>
-              {data.projects.map((project, index) => (
-                <Card
+            <div className="grid">
+              <Fade bottom cascade>
+                {data.projects.map((project, index) => (
+                  <Card
                   key={index}
                   heading={project.title}
                   paragraph={project.para}
                   imgUrl={project.imageSrc}
                   projectLink={project.url}
-                ></Card>
-              ))}
-            </Fade>
+                  ></Card>
+                  ))}
+              </Fade>
+            </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
--- src/components/skills.js
+++ src/components/skills.js
@@ -3,23 +3,23 @@ import data from "../yourdata"
 
 const Skills = () => {
   return (
-    <div className="section bg-image background">
-      <div id="stars" />
-      <div id="stars2" />
-      <div id="stars3" />
-      <div className="container">
-        <div className="skills-container">
-          <h1>Skills</h1>
-          <div className="skills-grid">
-            {data.skills.map((skill, index) => (
-              <div className="skill" key={index}>
-                <img src={skill.img} alt="css"></img>
-                <p>{skill.para}</p>
-              </div>
-            ))}
+    <div className="section bg-image background bg-responsive" id="skills">
+        <div id="stars" />
+        <div id="stars2" />
+        <div id="stars3" />
+        <div className="container">
+          <div className="skills-container">
+            <h1>Skills</h1>
+            <div className="skills-grid">
+              {data.skills.map((skill, index) => (
+                <div className="skill" key={index}>
+                  <img src={skill.img} alt="css"></img>
+                  <p>{skill.para}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
